35ca06840fe1cbf813f33b7746c65536f2068bc0
Henshuuz <henshuuz@henshuuz.dev>
2026-06-12 09:36:52 +0000

    fix: make local setup installable

--- store/settings.py
+++ store/settings.py
@@ -161,6 +161,8 @@ SESSION_COOKIE_AGE = 86400  # Cart lasts 24 hours (in seconds)
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
 
 # DRF default settings
